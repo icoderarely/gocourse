202df31cd2e432c7e01eb6851cbe779c9f713289
semicolon guy <itworksnavneet@gmail.com>
2025-10-10 14:45:28 +0530

    variables intro

---
+++ basics/04-variables.go
@@ -0,0 +1,14 @@
+package basics
+
+func main() {
+	// with `var` -> var variablename type = value
+	// if we not define a type, it will infer from the value but then value should be must
+	// can be used for global variable declaration
+	var name string = "Name"
+
+	// using the sign `:=` -> has to have a value; type is inferred from the value
+	// this can only be used within functions
+	name1 := "Name1"
+
+	// variables are block scoped and lives within scope
+}
