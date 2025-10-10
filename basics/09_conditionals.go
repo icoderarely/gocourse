187c8f25f6dac8079fec31111771b2356eb86309
semicolon guy <itworksnavneet@gmail.com>
2025-10-10 20:23:36 +0530

    conditionals

---
+++ basics/09_conditionals.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+	age := 23
+
+	// if else
+	// if age >= 18 {
+	// 	fmt.Println("Adult")
+	// } else if age < 14 {
+	// 	fmt.Println("Universal")
+	// } else {
+	// 	fmt.Println("Universal Adult")
+	// }
+
+	// and -> &&; or -> ||
+
+	// switch statement -> in go we dont need to use break statement at the end on cases
+	// switch expression {
+	// case value1:
+	// 	// code to be executed
+	// case valuen: ...
+	// default: // if no case matches
+	// }
+
+	// to make the switch behave as default like in other languages and pass to the next case, we can use `fallthrough;`
+
+	// 1. multiple conditions -> case "mon", "tue", "wed": ...code
+	// 2. case with expression ->
+	// 	a. boolean: switch { case num < 10: ...code }
+	// 	b. expression: switch num { case 1: ...cdoe }
+	// 3. switch with expression -> switch num / 2 {...code}
+}
